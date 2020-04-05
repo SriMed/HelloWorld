a5f08381d858f3227d62752e4537c0fc28667020
Srilakshmi Medarametla <bawesomesri2@outlook.com>
2020-04-04 23:24:47 -0400

    idk?

---
+++ app.js
@@ -0,0 +1,42 @@
+#!/usr/bin/nodejs
+
+
+// -------------- load packages -------------- //
+var express = require('express')
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var request = require('request');
+var path = require('path')
+var hbs = require('hbs');
+// var fs = require('fs');
+var cookieParser = require('cookie-parser')
+
+var app = express();
+
+var routes = require('./routes');
+// -------------- express initialization -------------- //
+app.set('trust proxy', 1) //trust first proxy?
+app.set('port', process.env.PORT || 8080 );
+
+// tell express that the view engine is hbs
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials')
+
+app.use(cookieParser('f92nfl29dfjl23t'))
+app.use(express.static(__dirname + '/static'));
+
+
+// io.on('connection', function(socket){
+//   console.log('a user connected');
+// });
+
+//express 'get' handles inside
+routes.doSet(app);
+
+// WILDCARD HANDLERS MUST COME AFTER ALL OTHER EXPLICIT ENDPOINTS 
+// -------------- listener -------------- //
+// The listener is what keeps node 'alive.' 
+
+var listener = app.listen(app.get('port'), function() {
+  console.log( 'Express server started on port: '+listener.address().port );
+});
